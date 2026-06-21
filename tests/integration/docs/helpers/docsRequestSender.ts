09f327638a293d281ad815ef7169f439f8e22db3
eyal rozen <eyalr1100@gmail.com>
2026-06-21 20:20:56 +0300

    fix: import supertest correctly

--- tests/integration/docs/helpers/docsRequestSender.ts
+++ tests/integration/docs/helpers/docsRequestSender.ts
@@ -1,13 +1,14 @@
-import * as supertest from 'supertest';
+import supertest, { type Response } from 'supertest';
+import type { Application } from 'express';
 
 export class DocsRequestSender {
-  public constructor(private readonly app: Express.Application) {}
+  public constructor(private readonly app: Application) {}
 
-  public async getDocs(): Promise<supertest.Response> {
+  public async getDocs(): Promise<Response> {
     return supertest.agent(this.app).get('/docs/api/');
   }
 
-  public async getDocsJson(): Promise<supertest.Response> {
+  public async getDocsJson(): Promise<Response> {
     return supertest.agent(this.app).get('/docs/api.json');
   }
 }
